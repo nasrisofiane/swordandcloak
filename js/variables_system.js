759e744597ad09e25fc524ce4969e1b3e362fee9
Dixen7 <dixenlive@gmail.com>
2019-08-21 12:29:19 +0200

    ajout de 12 items dans le shop plus affichage des niveau item sur le jeu

--- js/variables_system.js
+++ js/variables_system.js
@@ -55,12 +55,24 @@ const monsters = [ // tableau des noms de monstres et infos
 //3.0 Variables de fonctionnement de la boutique.
 var itemNumber = 0; //Ce nombre représente le nombre d'items créer dans la boutique, est incrémenté à chaque création d'item et fournit un itemID à chaque item.
 var itemsShop = [//Contient les informations des objets(boutique) en forme de tableau multidimensionn el.
-    ['Le Cure-dent', 500, 1, 1, "sword1", "epee",1],//(nom, prix, strenght, stamina ,image, type, bonus ), type = epee ou cape, bonus c'est les degats en + ou la vie en + en fonction de son type.
-    ['La Fisteuse', 5000, 40, 190, "sword0", "epee",200],
-    ['La Defourailleuse de Fion', 50000, 499, 501, "sword2", "epee",500],
+    ['Le Cure-dent', 500, 1, 1, "sword0", "epee",1],//(nom, prix, strenght, stamina ,image, type, bonus ), type = epee ou cape, bonus c'est les degats en + ou la vie en + en fonction de son type.
+    ['La Fisteuse', 5000, 40, 190, "sword1", "epee",100],
+    ['La Defourailleuse de Fion', 15000, 150, 280, "sword2", "epee",200],
+    ['L Ecarteuleuse', 30000, 399, 501, "sword3", "epee",400],
+    ['La Fendanus', 50000, 600, 740, "sword4", "epee",600],
+    ['La Douce Pin', 100000, 1000, 900, "sword5", "epee",800],
+    ['La Hrustchnurps', 180000, 1350, 1000, "sword6", "epee",1000],
+    ['La Aie Sapik', 250000, 2000, 1200, "sword7", "epee",1500],
+    ['Le Demon De Minuit', 666666, 6666, 6666, "sword8", "epee",2666],
     ['Cape Haute', 500, 6, 1, "cloak0", "cloak",110],
     ['Cape Oral', 1800, 27, 4, "cloak1", "cloak",240],
-    ['Cape Cake', 4700, 68, 12, "cloak2", "cloak",451]
+    ['Cape Cake', 4700, 68, 12, "cloak2", "cloak",451],
+    ['Cape Pougne', 8000, 180, 35, "cloak3", "cloak",650],
+    ['Cape Risti', 10000, 300, 90, "cloak4", "cloak",800],
+    ['Cape Rice Des Dieux', 15000, 600, 150, "cloak5", "cloak",1000],
+    ['Cape Ayrokei', 22000, 850, 250, "cloak6", "cloak",1200],
+    ['Cape Udepied', 28000, 1000, 400, "cloak7", "cloak",1500],
+    ['Cape De Staline', 40000, 1500, 869, "cloak8", "cloak",2000]
 ];
 var itemsShopBought = [];
 var incremenationItemShopBought = 0;
